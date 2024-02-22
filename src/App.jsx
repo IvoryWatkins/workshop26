a341c6701ad5525651da716a05bf81c010fe89fa
IvoryWatkins <ivory.watkins23@gmail.com>
2024-02-21 22:13:29 -0600

    adding workshop26, left off on Finishing Up tab

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,17 @@
-import { useState } from 'react'
-import './App.css'
-
+import './App.css';
+import { useState } from 'react';
+import ContactList from './components/Contactlist';
+import SelectedContact from './components/SelectedContact'
 
 function App() {
-  
-  return (
-    <>
-      
-    </>
-  )
-}
+  const [slectedContactId, setSelectedContactID] = useState(null);
 
-export default App
+  return(
+    <>
+    {setSelectedContactID ? <SelectedContact SelectedContactId={SelectedContactId} setSelectedContactID={setSelectedContactID}/> :
+    <div>Selected Contact View</div>}:{<ContactList setSelectedContactID={setSelectedContactID} />}
+    </>
+  );
+  } 
+  
+export default App;  
